7dde62cd7270569295eec09e6a414b19e112af6a
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-06-29 01:32:17 +0530

    tag changed

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def delete_file(filename):
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
